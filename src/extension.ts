b0a39b3d6a97d283d37ee0ab3c77fb84e618f99c
Colby Timm <timmcolby@gmail.com>
2025-06-18 21:10:41 -0700

    Add Yarn configuration and update package dependencies
    - Create .yarnrc and .yarnrc.yml files for Yarn configuration.
    - Update package.json description and VS Code engine version.
    - Adjust import path in extension.ts for consistency.
    - Update @types/vscode version in yarn.lock.

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,5 @@
 import * as vscode from 'vscode';
-import { Participant } from 'src/utils/register.util';
+import { Participant } from '@utils/register.util';
 
 export async function activate(context: vscode.ExtensionContext) {
     const participant = new Participant();
